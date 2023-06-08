213782e51d2e3f02968dfca852fd7209e5b2a098
codingking02 <zeyadismailo2002@gmail.com>
2023-06-09 01:54:22 +0300

    changes i made was in the main

--- lib/main.dart
+++ lib/main.dart
@@ -7,49 +7,52 @@ import 'package:provider_text/provider/proxyprovider/greetingserviceprovider.dar
 import 'package:provider_text/provider/savingdataprovider.dart';
 import 'package:provider_text/provider/proxyprovider/userprovider.dart';
 import 'package:provider_text/provider/valuelistnableprovider.dart';
-import 'package:provider_text/screens/futureproviderscreen.dart';
-import 'package:provider_text/screens/proxy_home_ui.dart';
 import 'package:provider_text/screens/proxy_login_ui.dart';
 import 'package:provider_text/screens/streamproviderscreen.dart';
 
 //Future Builed make an example
 void main() {
-  Future<Future_Provider> getprovider() async {
-    Future.delayed(
-      Duration(seconds: 5),
-    );
-    return await Future_Provider("helloz");
-  }
+  runApp(MyApp());
+}
 
-  Stream<Future_Provider> getstreamprovider() {
-    return Stream<Future_Provider>.periodic(
-      const Duration(
-        seconds: 5,
-      ),
-      (computationCount) {
-        return Future_Provider("$computationCount");
-      },
-    ).take(10);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  runApp(
-    MultiProvider(
+  @override
+  Widget build(BuildContext context) {
+    Future<Future_Provider> getprovider() async {
+      Future.delayed(
+        Duration(seconds: 5),
+      );
+      return await Future_Provider("helloz");
+    }
+
+    Stream<Future_Provider> getstreamprovider() {
+      return Stream<Future_Provider>.periodic(
+        const Duration(
+          seconds: 5,
+        ),
+        (computationCount) {
+          return Future_Provider("$computationCount");
+        },
+      ).take(10);
+    }
+
+    //String mytext = context.read<TextProvider>().text;
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (context) => TextProvider(),
-          child: MyApp(),
         ),
         ProxyProvider<UserProvider, GreetingsProvider>(
           update: (context, _value, previous) {
             return GreetingsProvider(userProvider: _value);
           },
-          child: Home(),
         ),
         ProxyProvider2<UserProvider, GreetingsProvider, CartProvider>(
           update: (context, value, value2, previous) {
             return CartProvider(greetingsProvider: value2, userProvider: value);
           },
-          child: Home(),
         ),
         Provider(
           create: (context) => UserProvider(),
@@ -65,7 +68,6 @@ void main() {
             return getprovider();
           },
           initialData: Future_Provider("helloz"),
-          child: FutureProvider_ui(),
         ),
         StreamProvider(
           create: (context) => getstreamprovider(),
@@ -73,20 +75,15 @@ void main() {
           child: StreamProvider_ui(),
         ),
       ],
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //String mytext = context.read<TextProvider>().text;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: login(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const login(),
+      ),
     );
   }
 }
